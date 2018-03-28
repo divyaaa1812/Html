879831ed55b8c660a9927519a8c3ef13ff02303a
Divya Bharathi <divyaaa1812@gmail.com>
2018-03-27 22:28:10 -0700

    Html

---
+++ script.js
@@ -0,0 +1,16 @@
+$('#para').click(function() {
+      $('#para').hide();
+});
+
+$(document).ready(function(){
+  $('#para1').fadeIn(4000);
+  //fadeIn('fast'); fadeIn(3000);
+});
+
+$('window').load(function(){
+  alert('images have loaded');
+});
+
+$(document).ready(function(){
+  alert("Dom is ready");
+});
